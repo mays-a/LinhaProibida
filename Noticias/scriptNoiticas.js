5cbd6647e62ced610aaf84a4f4188d309c29870c
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-12-08 22:41:09 -0300

    Delete Noticias directory

--- Noticias/scriptNoiticas.js
+++
@@ -1,6 +0,0 @@
-const menuOpenbutton = document.querySelector("#menu-open-button");
-
-menuOpenbutton.addEventListener("click", () => {
-    // Toglle mobile menu visivility
-    document.body.classList.toggle("show-mobile-menu");
-});
